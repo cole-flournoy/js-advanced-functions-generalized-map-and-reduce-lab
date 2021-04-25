da02b342ac2060c512aa0a98fb1f25a92f8b96a4
Cole Flournoy <flournoy.cole@gmail.com>
2021-04-25 15:49:03 -0400

    commit tree 4414e1ff42f336073d37679c6f96cb572bf3e41d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(sourceArray, callback){
+  const newArray = []
+  for (let element of sourceArray){
+    newArray.push(callback(element))
+  } 
+  return newArray
+}
+
+function reduce(sourceArray, callback, startingPoint){
+  let total
+  if (startingPoint){
+    total = startingPoint
+    for (let element of sourceArray){
+      total = callback(element, total)
+    }
+    return total
+  } else {
+    total = sourceArray[0]
+    for (let i = 1; i < sourceArray.length; i++) {
+      total = callback(sourceArray[i], total)
+    }
+  return total
+  }
+}
